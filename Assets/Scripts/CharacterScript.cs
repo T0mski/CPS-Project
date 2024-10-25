43fc01fb11766b4baa61a4724417b58eb818e2ae
T0mski <tomski2007@icloud.com>
2024-10-25 12:43:59 +0100

    Commented the Character script

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -8,18 +8,21 @@ using UnityEngine.InputSystem.XR.Haptics;
 
 public class CharacterScript : MonoBehaviour
 {
-    public Transform target;
-    public float speed;
-    private Vector3 targetpos;
-    public GameObject tower;
-    private bool colliding = false;
-    private Vector3 ThisObject;
+    
+    public Transform target; // This stores the Target Location for the player character to go towards.
+    public float speed; // speed constant
+    private Vector3 targetpos; // the 3D Vector of the locatiom that you want the player to move towards.
+    public GameObject tower; // the game object of the next building to get the target.
+    private bool colliding = false; // Whether or not the player is colliding witht the ground.
+    private Vector3 ThisObject; // A variable of the 3D Vector of this game objects location.
 
+    // This function is called at the start of the game and only at the start of the game. 
     private void Start()
     {
         target = tower.transform;
         GetTargetPos();
     }
+    //This is the function that is called every game frame.
     private void Update()
     {
         if (!colliding)
@@ -29,7 +32,7 @@ public class CharacterScript : MonoBehaviour
         
              
     }
-    
+    // sets the target position of the player so that it can move to the next building. 
     public void GetTargetPos()
     {
         if (target != null)
@@ -38,26 +41,25 @@ public class CharacterScript : MonoBehaviour
             targetpos.y += tower.transform.position.y + GameObject.Find("Character").transform.localScale.y;
         }
     }
-
+    // the function that is called when the player moves towards the targetpos
    public void PlayerMove()
     {
         float step = speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, targetpos, step);
-        gravity();
         if (transform.position == targetpos)
         {
             GameObject.Find("PoleCollider").GetComponent<PoleCollisionScript>().CollisionCheck = false;
         }
     }
     
-
+    // moves the player down by a specific amount of pixels every frame. Acts as gravity.
     private void gravity()
     {
         ThisObject = transform.position;
         ThisObject.y += -9.8f * Time.deltaTime;
         transform.position = ThisObject;
     }
-
+    //Checks the collisions between the two game objects.
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject != null)
