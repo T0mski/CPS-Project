e0f5d75b0e30a25fd061785c4546fe193ae3091d
T0mski <thomaswaddington1@gmial.com>
2024-11-06 22:18:35 +0000

    Started on the Building Spawner

--- Assets/Buildings/BuildingSpawner.cs
+++ Assets/Buildings/BuildingSpawner.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO.Pipes;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class BuildingSpawner : MonoBehaviour
@@ -9,27 +11,11 @@ public class BuildingSpawner : MonoBehaviour
     public float spawnrate = 2;
     private float timer = 0;
 
-    private void Start()
-    {
-        SpawnBuilding();
-    }
-    private void Update()
-    {
-        if (timer < spawnrate)
-        {
-            timer += Time.deltaTime;
-        }
-        else
-        {
-            SpawnBuilding();
-            timer = 0;
-        }
-        
-    }
-
-
     void SpawnBuilding()
     {
         Instantiate(Building, transform.position, transform.rotation);
     }
+
+
+
 }
