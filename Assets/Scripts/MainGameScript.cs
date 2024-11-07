a28ab044981367e6412d7ad57fb2ead5ef10cfd0
T0mski <thomaswaddington1@gmial.com>
2024-11-07 21:23:32 +0000

    Made the random scaling and building spawn
    now need to make the spawn on correct hit.

--- Assets/Scripts/MainGameScript.cs
+++ Assets/Scripts/MainGameScript.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class MainGameScript : MonoBehaviour
 {
     public GameObject Building;
-    
-
+    private float[] nextMult = new float[7]  {1f, 1.5f, 0.3f, 0.5f, 1.3f, 1.8f, 0.7f};
+    private int RandomNum;
+    private float Multiplyer;
+    private float nextScaleX;
 
     void SpawnBuilding()
     {
+        RandomNum = Random.Range(1, nextMult.Length);
+        Multiplyer = nextMult[RandomNum];
+        
+        Vector3 scale = Building.transform.localScale;
+        scale.x = scale.x * Multiplyer;
+        transform.localScale = scale;
         Instantiate(Building, transform.position, transform.rotation);
     }
 }
